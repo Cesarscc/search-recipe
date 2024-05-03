a7c9e328c894c5bc48102179f8af4995f26ddbff
@Cesarscc Cesarscc <ccoloradoc@uni.pe>
2024-05-02 19:07:23 -0500

    fix: close-modal

--- src/components/ModalUnSaved.js
+++ src/components/ModalUnSaved.js
@@ -4,10 +4,9 @@ import { useEffect } from "react";
 const ModalUnSaved = ({ closeModalAlert }) => {
   useEffect(() => {
     return () => {
-      // Aquí cerramos el modal después de 5 segundos
       setTimeout(closeModalAlert, 1500);
     };
-  }, [closeModalAlert]);
+  }, []);
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-40">
